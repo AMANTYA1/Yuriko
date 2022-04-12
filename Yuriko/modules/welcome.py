5d93f51b967bd2cc3e8cf788192db8198f4edffb
ALONE BOY <84889578+PRONOI@users.noreply.github.com>
2022-04-12 12:25:12 +0530

    Update welcome.py

--- Yuriko/modules/welcome.py
+++ Yuriko/modules/welcome.py
@@ -1267,7 +1267,7 @@ __help__ = """
 
 ✗ /welcomehelp - `view more formatting information for custom welcome/goodbye messages.`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: BᴏᴛDᴜɴɪʏᴀ!*
 """
 
 NEW_MEM_HANDLER = MessageHandler(
