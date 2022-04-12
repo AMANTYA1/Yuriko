fa61eb40878532399c408856b4fb0c48d3aa603b
ALONE BOY <84889578+PRONOI@users.noreply.github.com>
2022-04-12 12:38:38 +0530

    Update gtranslator.py

--- Yuriko/modules/gtranslator.py
+++ Yuriko/modules/gtranslator.py
@@ -45,7 +45,7 @@ ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
 sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
 vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: BᴏᴛDᴜɴɪʏᴀ!*
 """
 
 __mod_name__ = "Tʀᴀɴꜱʟᴀᴛᴏʀ"
