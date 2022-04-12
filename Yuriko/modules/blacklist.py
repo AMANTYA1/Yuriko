71f865dfb688d7bf8a0d44b5604106fd7e46e3bf
ALONE BOY <84889578+PRONOI@users.noreply.github.com>
2022-04-12 12:46:02 +0530

    Update blacklist.py

--- Yuriko/modules/blacklist.py
+++ Yuriko/modules/blacklist.py
@@ -487,7 +487,7 @@ Note:
 
 ✗ *<sticker link> can be* `https://t.me/addstickers/<sticker>` *or just* `<sticker>` *or reply to the sticker message*
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: BᴏᴛDᴜɴɪʏᴀ!*
 """
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
     "blacklist",
